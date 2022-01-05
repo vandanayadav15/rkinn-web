d42be3c3941253da1e55cdfdf5bc9b1f62d0420e
Vandana Yadav <vy720751@gmail.com>
2022-01-05 16:16:39 +0530

    replaced logo in png

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,12 +1,36 @@
 import { Container, Typography } from "@mui/material";
 import React from "react";
 
+
+const styles:any = {
+  paperContainer: {
+    backgroundImage: `url(${"../../../hotel_banner.jpeg"})`,
+    height: "600px",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "100% 100%",
+    marginTop: "60px",
+  },
+};
+
 const Banner = () => {
   return (
-  <Container sx={{display:"flex", alignItems: "center", height: "700px"}} >
-    <Typography variant="h1" color="primary">
+    <Container sx={{ display: "flex", alignItems: "center", height: "700px" }} >
+      <Typography
+        variant="h1"
+        color="primary"
+      >
         Page Under Contruction
-    </Typography>
+      </Typography>
+      <Typography
+        variant="h6"
+        component="div"
+        gutterBottom
+        align="center"
+        data-aos="zoom-in-up"
+        sx={{ padding: "20"}}
+      >
+      Indane has now become one of the largest packed-LPG brands in the world with IndianOil as the second 
+        </Typography>
   </Container>
   );
 }
