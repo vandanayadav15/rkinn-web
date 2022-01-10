988042dca94158bc23b32b1997b2d2e11dffce9e
Vandana Yadav <vy720751@gmail.com>
2022-01-10 16:43:06 +0530

    added border radius

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -1,52 +1,48 @@
-
-import { Box, Grid, Paper, styled, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import Image from "next/image"
-import React from 'react'
-
+import { Box, Grid, Paper, styled, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import Image from "next/image";
+import React from "react";
 
 const holtes = [
   {
-        id: 1,
-        image: "/img/banner/1-xl.jpeg",
- },
+    id: 1,
+    image: "/img/banner/1-xl.jpeg",
+  },
   {
-      id: 2,
-      image: "/img/banner/2-xl.jpeg",
-    },
+    id: 2,
+    image: "/img/banner/2-xl.jpeg",
+  },
   {
-      id: 3,
-        image: "/img/banner/3-xl.jpeg",
-    },
+    id: 3,
+    image: "/img/banner/3-xl.jpeg",
+  },
 ];
 
-
-const About = (props:any) => {
-    return (
-        <>
-             <br />
+const About = (props: any) => {
+  return (
+    <>
+      <br />
       <Typography
         variant="h4"
         gutterBottom
         component="div"
         sx={{ textAlign: "center" }}
-                color={'primary'}
-                 >
-                Where do you want to go?
-            </Typography>
+      >
+        Where do you want to go?
+      </Typography>
 
-        <Typography
-        variant="h6"
+      <Typography
+        variant="body1"
         gutterBottom
         component="div"
         sx={{ textAlign: "center" }}
-        color={'primary'} >
+      >
         We recommend you the next destinations witha sweet discounted price
-            </Typography>
-            
-       <br />
-      <Grid container spacing={4}>
-        {holtes.map((holtes,index) => {
+      </Typography>
+
+      <br />
+      <Grid container spacing={6}>
+        {holtes.map((holtes, index) => {
           return (
             <Grid
               key={index}
@@ -58,41 +54,25 @@ const About = (props:any) => {
               sx={{ textAlign: "center", padding: "8px" }}
             >
               <Box
-                      key={holtes.id}
-                      
-                style={{
-                //   padding: "30px",
-                //   background: "#ebebeb",
-                //   height: "360px
-                }}
-                // data-aos="fade-up"
-                // data-aos-delay={200*index}
-                  >
-                <Image
-                  src={holtes.image}
-                  alt=""
-                              width="300px" height="300px" 
-                               data-aos="fade-up"
-                               data-aos-anchor-placement="top-bottom"
-                />
-                {/* <Typography variant="h6" gutterBottom component="div" color="primary">
-                  {holtes.text}
-                </Typography>{" "}
-                <br /> */}
-                <Typography
-                  variant="body1"
-                  sx={{ textAlign: "left" }}
-                  gutterBottom
-                  component="div"
+                key={holtes.id}
+                style={{ borderRadius: "16px", overflow: "hidden" }}
+              >
+                <div
                   style={{
-                    padding: "0px 0px",
-                    letterSpacing: "1px",
-                    textAlign: "justify",
+                    position: "relative",
+                    borderRadius: "16px",
+                    overflow: "hidden",
                   }}
                 >
-                  {/* {holtes.para} */}
-                          </Typography>{" "}
-                          
+                  <Image
+                    src={holtes.image}
+                    alt=""
+                    width="400px"
+                    height="300px"
+                    data-aos="fade-up"
+                    data-aos-anchor-placement="top-bottom"
+                  />
+                </div>
               </Box>
             </Grid>
           );
@@ -100,10 +80,7 @@ const About = (props:any) => {
       </Grid>
       <br />
       <br />
-           
-        </>
-    )
-}
+    </>
+  );
+};
 export default About;
-
-
